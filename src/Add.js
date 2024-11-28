8631598c0dda73c3fb6d830f233e7cbf420a684b
Yadav.anand <anandjhansi@gmail.com>
2024-11-28 10:34:11 +0530

    StringCalculator: Number addition logic added.

--- src/Add.js
+++ src/Add.js
@@ -1,7 +1,14 @@
 function add(numbers) {
-    if (!numbers) {
-      return 0;
+    let result = 0; delimiter = ",";
+    if (numbers) {
+      const numbersToAdd = numbers.split(delimiter);
+      if(numbersToAdd.length > 0) {
+        result = numbersToAdd.reduce((total, number) => {
+          return total + Number(number);
+        }, 0);
+      }
     }
+    return result;
 }
 
 module.exports = add;
