7e0066fec0eab344b662f332711dac73d9a8babc
Yadav.anand <anandjhansi@gmail.com>
2024-11-28 09:55:23 +0530

    StringCalculator: Initial functionality added

---
+++ README.md
@@ -0,0 +1,24 @@
+# STRING CALCULATOR
+
+## PRE-REQUISITE:
+
+Below softwares should be installed:
+
+- Node 18 or above
+- NPM (Node package manager)
+
+## Installation
+
+To install and run String Calculator locally, follow these steps:
+
+1. **Clone the repositiory:**
+
+```sh
+git clone URL
+cd StringCalculator
+
+2. ** Install dependencies **
+   npm install
+
+3. ** Run test cases **
+    npm run test
---
+++ tests/add.test.js
@@ -0,0 +1,29 @@
+const add = require("../src/Add");
+
+describe("String Calculator TDD", () => {
+    test("returns 0 if no string passed as input", () => {
+        expect(add()).toBe(0);
+    });
+    
+    test("returns 0 if empty string gets passed", () => {
+        expect(add("")).toBe(0);
+    });
+
+    test("should return same number if passed single number", () => {
+        expect(add("1")).toBe(1);
+        expect(add("3")).toBe(3);
+    });
+
+    test("should return SUM of all passed numbers by default comma seperated", () => {
+        expect(add("1,2,3")).toBe(6);
+        expect(add("0,3,5")).toBe(8);
+    });
+
+    test("should accept new line character", () => {
+        expect(add("1\n,2,3")).toBe(6);
+    });
+    
+    test("should accept new delimiter starting with //", () => {
+        expect(add("//;1\n;2;3")).toBe(6);
+    });
+});
